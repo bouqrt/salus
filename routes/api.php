424e2f04da5dee162a2c28f94b7370d48370900e
bouchra <bouchraelqrt@gmail.com>
2026-03-16 22:38:16 +0000

    route: define API endpoints for auth, symptoms, doctors and appointments

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,39 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SymptomsController;
+use App\Http\Controllers\DoctorController;
+use App\Http\Controllers\AppointmentController;
+
 
 Route::get('/user', function (Request $request) {
+
     return $request->user();
 })->middleware('auth:sanctum');
+Route::post(
+    "logout",
+    [AuthController::class, "logout"]
+)->middleware('auth:sanctum');
+
+Route::post('register', [AuthController::class, "create"]);
+Route::post('login', [AuthController::class, "login"]);
+
+
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::apiResource('symptoms', SymptomsController::class);
+    Route::apiResource('appointments', AppointmentController::class)->except(['update']);
+    Route::get('/doctors', [DoctorController::class, 'index']);
+    Route::get('/doctors/search', [DoctorController::class, 'search']);
+    Route::get('/doctors/{id}', [DoctorController::class, 'show']);
+
+
+    Route::get('/me', function (Request $request) {
+        return response()->json([
+            'success' => true,
+            'data' => $request->user()
+        ]);
+    });
+});
