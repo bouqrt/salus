60511a0f596b0186320fa7ca18e48f1dfd9e8afe
bouchra <bouchraelqrt@gmail.com>
2026-03-16 22:38:39 +0000

    feat: implement doctor listing and search by specialty or city

---
+++ app/Http/Controllers/DoctorController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Doctor;
+use Illuminate\Http\Request;
+use App\Traits\ApiResponse;
+
+
+class DoctorController extends Controller
+{
+    use ApiResponse;
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+        return $this->successResponse(Doctor::all(),"liste des medecins");
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show($id)
+    {
+        //
+        $doctor=Doctor::findOrFail($id);
+        return $this->successResponse($doctor,"details de medecin");
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Doctor $doctor)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Doctor $doctor)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Doctor $doctor)
+    {
+        //
+    }
+
+    public function search(Request $request){
+        $query=Doctor::query();
+        if($request->has('specialty')){
+            $query->where('specialty', 'like', '%'.$request->specialty.'%');
+
+        }
+         if($request->has('city')){
+            $query->where('city', 'like', '%'.$request->specialty.'%');
+
+        }
+        $results=$query->get();
+        return $this->successResponse($results,"Resultats de la rechherche ");
+
+    }
+}
